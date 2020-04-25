c5c5c5a4886e2856e94654fb3e3e1d82bd2ec178
David Behar <davbelom@gmail.com>
2020-04-24 22:34:41 -0500

    🏗 (Global): Connection with whatsapp
    Full Connection with whatsapp and send messsages to clientes

---
+++ src/mocks/clients.js
@@ -0,0 +1,19 @@
+const clients = [{
+  fullName: 'Nombre Completo',
+  name: 'David',
+  fullName2: 'Nombre Completo',
+  tienda: '',
+  adicional: 'Info adicional',
+  estatus: '',
+  usted: false,
+  prefix: '',
+  send: false,
+  received: false,
+  replied: false,
+  displayName: 'Nombre completo',
+  phone: 5555555555,
+  sent: false,
+},
+];
+
+module.exports = clients;
---
+++ src/util/getMessages.js
@@ -0,0 +1,36 @@
+const messageUsted = `
+¿Cómo se encuentra?
+
+Le quería platicar que estoy recibiendo mercancía, por lo que estoy lista para el día de las madres.
+
+Estoy surtiendo pedidos desde mi oficina en casa, por lo que me puede pedir lo que quiera del catálogo y yo se lo envío.
+
+Le recomiendo hacer su pedido rápido para que no se vean muy saturadas las paqueterías y no se agoten los mejores productos.
+
+Un fuerte abrazo.`;
+const messageTu = `
+
+¿Cómo te encuentras?
+
+Te quería platicar que estoy recibiendo mercancía, por lo que estoy lista para el día de las madres.
+
+Estoy surtiendo pedidos desde mi oficina en casa, por lo que me puedes pedir lo que quieras del catálogo y yo te lo envío.
+
+Te recomiendo hacer tu pedido rápido para que no se vean muy saturadas las paqueterías y no se agoten los mejores productos.
+
+Un fuerte abrazo.
+`;
+
+const getMessage = (name, usted, prefix) => {
+  let nombre = name;
+  if (usted) {
+    if (prefix === 'Sr') {
+      nombre = `Sr ${name}`;
+    } else {
+      nombre = `Sra ${name}`;
+    }
+  }
+  return `Hola ${nombre} ${usted ? messageUsted : messageTu}`;
+};
+
+module.exports = getMessage;
